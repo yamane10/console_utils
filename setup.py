25fb4f5790a607bc97d5eb4b2accc6101e94ac09
Josh Yamane <yamane10@gmail.com>
2019-10-10 18:03:39 -0500

    Collapsed package into single module, and added setup.py and install.sh

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "console_utils",
+	version = "0.1.0",
+	author="Josh Yamane",
+	author_email="joshua.yamane@kwe.com",
+	py_modules = ["console_utils"]
+	)
